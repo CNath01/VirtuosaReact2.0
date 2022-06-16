a9589850b3fd51b9ca13b24690f9b47c16a50925
CNath01 <camnath0102@gmail.com>
2022-06-16 01:02:59 -0500

    Prueba :(

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import Inicio from './pages/Inicio';
+
+import Error404 from './pages/Error404';
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  <BrowserRouter>
+    <Routes>
+      <Route path='/virtuosaReact/' element={<Inicio />} ></Route>
+     
+      <Route path='*' element={<Error404 />}></Route>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <Route path='/home' element={<Navigate replace to={"/"} />}></Route>
+
+    </Routes>
+  </BrowserRouter>
+
+);
